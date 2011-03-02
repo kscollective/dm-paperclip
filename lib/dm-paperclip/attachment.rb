60978d66bfeaf7c8741bbbfd2eef4aa219b8980c
Jacob Basham <jacob@paperpigeons.net>
2011-03-01 22:32:04 -0600

    Go back to normal content type

--- lib/dm-paperclip/attachment.rb
+++ lib/dm-paperclip/attachment.rb
@@ -88,8 +88,8 @@ module Paperclip
       else
         @queued_for_write[:original]   = to_tempfile(uploaded_file)
         instance_write(:file_name,       uploaded_file.original_filename.strip.gsub(/[^\w\d\.\-]+/, '_'))
-        #instance_write(:content_type,    uploaded_file.content_type.to_s.strip)
-        instance_write(:content_type,    content_type(uploaded_file.original_filename))
+        instance_write(:content_type,    uploaded_file.content_type.to_s.strip)
+        #instance_write(:content_type,    content_type(uploaded_file.original_filename))
         instance_write(:file_size,       uploaded_file.size.to_i)
         instance_write(:updated_at,      Time.now)
       end
