46791332cbbb57b6512383d702e35a2202fe586a
Jacob Basham <jacob@paperpigeons.net>
2011-03-01 22:22:33 -0600

    Fix syntax error

--- lib/dm-paperclip/attachment.rb
+++ lib/dm-paperclip/attachment.rb
@@ -89,7 +89,7 @@ module Paperclip
         @queued_for_write[:original]   = to_tempfile(uploaded_file)
         instance_write(:file_name,       uploaded_file.original_filename.strip.gsub(/[^\w\d\.\-]+/, '_'))
         #instance_write(:content_type,    uploaded_file.content_type.to_s.strip)
-        instance_write(:content_type,    content_type(uploaded_file.original_filename)
+        instance_write(:content_type,    content_type(uploaded_file.original_filename))
         instance_write(:file_size,       uploaded_file.size.to_i)
         instance_write(:updated_at,      Time.now)
       end
